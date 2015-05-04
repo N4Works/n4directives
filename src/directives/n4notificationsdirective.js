adb5851dca9a70c0b56de7afe3d91f282d27dd05
Tiago Resende <tiagor87@gmail.com>
2015-05-04 14:57:04 -0300

    Implementação da diretiva de notificações.

--- src/directives/n4notificationsdirective.js
+++ src/directives/n4notificationsdirective.js
@@ -5,10 +5,36 @@
     .run([
       '$templateCache',
       function ($templateCache) {
-        $templateCache.put('information.html');
-        $templateCache.put('alert.html');
-        $templateCache.put('success.html');
-        angular.element('body').append('<n4-notifications></n4-notifications>');
+        $templateCache.put('success.html', [
+          '<div class="notification success">',
+          '  <div class="text" data-ng-bind="notification.message">',
+          '  </div>',
+          '  <div class="actions">',
+          '    <a class="secondary" href="" data-ng-if="notification.secondaryButtonText" data-ng-bind="notification.secondaryButtonText" data-ng-click="notification.callback(notification.secondaryButtonText)"></a>',
+          '    <a class="primary" href="" data-ng-bind="notification.primaryButtonText" data-ng-click="notification.callback(notification.primaryButtonText)"></a>',
+          '  </div>',
+          '</div>'
+        ].join(''));
+        $templateCache.put('alert.html', [
+          '<div class="notification alert">',
+          '  <div class="text" data-ng-bind="notification.message"></div>',
+          '  <div class="actions">',
+          '    <a class="secondary" href="" data-ng-if="notification.secondaryButtonText" data-ng-bind="notification.secondaryButtonText" data-ng-click="notification.callback(notification.secondaryButtonText)"></a>',
+          '    <a class="primary" href="" data-ng-bind="notification.primaryButtonText" data-ng-click="notification.callback(notification.primaryButtonText)"></a>',
+          '  </div>',
+          '</div>'
+        ]);
+        $templateCache.put('information.html', [
+          '<div class="notification information">',
+          '  <div class="text" data-ng-bind="notification.message">',
+          '  </div>',
+          '  <div class="actions">',
+          '    <a class="primary" href="" data-ng-bind="notification.primaryButtonText" data-ng-click="notification.callback(notification.primaryButtonText)"></a>',
+          '  </div>',
+          '</div>'
+        ]);
+        angular.element('body')
+          .append('<n4-notifications></n4-notifications>');
       }
     ])
     .directive('n4Notifications', [
@@ -18,7 +44,11 @@
           restrict: 'E',
           replace: true,
           scope: {},
-          template: '<div class="notifications"><n4-notification ng-attr-template="{{notification.template}}" data-ng-repeat="notification in service.notifications"></n4-notification></div>',
+          template: [
+            '<div class="notifications">',
+            '  <n4-notification ng-attr-template="{{notification.template}}" data-ng-repeat="notification in service.notifications"></n4-notification>',
+            '</div>'
+          ].join(''),
           link: function (scope) {
             scope.service = service;
           }
