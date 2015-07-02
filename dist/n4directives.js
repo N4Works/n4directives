3ba2d971f7f113e72f61c4e2df18aecd306f16f9
Tiago <tiagor87@gmail.com>
2015-07-02 13:57:41 -0300

    Executa build no pacote.

--- dist/n4directives.js
+++ dist/n4directives.js
@@ -1,12 +1,68 @@
 (function (angular) {
-  'use strict';
+    'use strict';
 
-  angular.module('n4Directives', [
-    'n4Directives.numberInput',
-    'n4Directives.notifications',
-    'n4Directives.onBottom'
-  ]);
+    angular.module('n4Directives', [
+        'n4Directives.numberInput',
+        'n4Directives.dateInput',
+        'n4Directives.notifications',
+        'n4Directives.onBottom',
+        'n4Directives.interceptor'
+    ]);
 
+}(window.angular));
+
+(function (angular) {
+    'use strict';
+
+    /*jslint regexp: true */
+
+    angular.module('n4Directives.dateInput', [])
+        .directive('n4DateInput', [
+            '$filter',
+            function ($filter) {
+                return {
+                    require: 'ngModel',
+                    restrict: 'EAC',
+                    replace: true,
+                    template: '<input type="text">',
+                    link: function (scope, element, attrs, controller) {
+                        var getFormattedValue = function (value) {
+                                var date = new Date(value);
+                                if (isNaN(date)) {
+                                    return '';
+                                }
+
+                                return $filter('date')(date, 'dd/MM/yyyy');
+                            },
+                            parseValue = function (value) {
+                                if (!value) {
+                                    return null;
+                                }
+
+                                var date = new Date(value);
+                                if (isNaN(date)) {
+                                    var digits = value.replace(/\D/g, '');
+                                    date = new Date(digits.substr(2, 2) + '/' + digits.substr(0, 2) + '/' + digits.substr(4, 4));
+                                    if (isNaN(date)) {
+                                        return null;
+                                    }
+                                }
+
+                                var formmatedValue = getFormattedValue(date);
+                                controller.$setViewValue(formmatedValue);
+                                controller.$render();
+                                return date;
+                            };
+
+                        element.attr('placeholder','00/00/0000');
+                        element.attr('maxlength',10);
+
+                        controller.$formatters.unshift(getFormattedValue);
+                        controller.$parsers.unshift(parseValue);
+                    }
+                };
+            }
+        ]);
 }(window.angular));
 
 (function (angular, $) {
@@ -168,6 +224,36 @@
       }]);
 
 }(window.angular, window.jQuery));
+
+(function (angular) {
+    'use strict';
+
+    angular.module('n4Directives.interceptor', [])
+        .factory('n4Interceptor', [
+            '$q',
+            '$log',
+            function ($q, $log) {
+                var N4Interceptor = function (defaultErrorMessage) {
+                    this.defaultErrorMessage = defaultErrorMessage || 'An error has occurred. Try again later.';
+                };
+
+                N4Interceptor.prototype = {
+                    responseError: function (rejection) {
+                        if (rejection.data) {
+                            return $q.reject(new TypeError(rejection.data));
+                        }
+
+                        $log.error(rejection);
+                        return $q.reject(new TypeError(this.defaultErrorMessage));
+                    }
+                };
+
+                return function (defaultErrorMessage) {
+                    return new N4Interceptor(defaultErrorMessage);
+                };
+            }]);
+
+}(window.angular));
 
 (function (angular) {
   'use strict';
